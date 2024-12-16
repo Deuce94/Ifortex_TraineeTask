5d402790ac660dfe988959e672c34946823c977b
mazzz <mazzz@MIKHAIL97>
2024-12-16 20:43:58 +0300

    Add project files.

---
+++ TestTask/Services/Implementations/AuthorService.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class AuthorService : IAuthorService
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly int _publishedDateYear = 2015;
+        public AuthorService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<Author> GetAuthor()
+        {
+            Author author = (await _context.Books.Include(book => book.Author)
+            .OrderByDescending(book => book.Title.Length)
+            .ThenBy(book => book.AuthorId)
+            .FirstOrDefaultAsync())?.Author;
+
+            return new Author // если так не сделать будет зацикливоние json
+            {
+                Id = author.Id,
+                Surname = author.Surname,
+                Name = author.Name
+            };
+        }
+
+
+
+        public async Task<List<Author>> GetAuthors()
+        {
+            var authors = await _context.Books.Include(author => author.Author)
+                                .Where(book => book.PublishDate.Year > _publishedDateYear)
+                                .GroupBy(book => new { book.AuthorId, book.Author.Books.Count })
+                                .Where(groups => groups.Key.Count % 2 == 0)
+                                .Select(groups => groups.First().Author)
+                                .ToListAsync();
+            List<Author> authorsToDisplay = new(); // если так не сделать будет зацикливоние json
+            authors.ForEach(author => authorsToDisplay.Add(new Author { Id = author.Id, Name = author.Name, Surname = author.Surname }));
+            return authors;
+        }
+    }
+}
---
+++ TestTask/Services/Implementations/BookService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class BookService : IBookService
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly DateTime _sabatonCarolusRexAlbumPublishedDate = new DateTime(2012,5,22);
+
+        public BookService(ApplicationDbContext context)
+        {
+            _context = context;   
+        }
+        public async Task<Book> GetBook() =>       
+             await _context.Books.OrderByDescending(x => x.QuantityPublished * x.Price).FirstOrDefaultAsync();
+
+
+        public async Task<List<Book>> GetBooks() =>
+            await _context.Books.Where(book => book.PublishDate >= _sabatonCarolusRexAlbumPublishedDate && book.Title.Contains("Red")).ToListAsync();
+    }
+}
